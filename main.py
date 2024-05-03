1fba6ad54aed35cc8e9018256126d94300573062
Aleksa Tvrdisic <tvrdisica@gmail.com>
2024-05-03 19:13:41 +0200

    nato phonetic alphabet

---
+++ main.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dictionary = {row.letter: row.code for (index, row) in data.iterrows()}
+word = input("Enter a word: ").upper()
+
+result = [phonetic_dictionary[letter] for letter in word]
+print(result)
